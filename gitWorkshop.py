3a42f483ed8fdc805f66d24cf573c68c5f837a1c
eMu <emad.easa@codecool.com>
2022-05-06 11:30:24 +0200

    Generated code for generating python feedback file

--- gitWorkshop.py
+++ gitWorkshop.py
@@ -1,1 +1,27 @@
-print("Oh no, you got a virus!!!!!!!!!!!")
+import os
+
+print("Hi there - awaiting your feedback in form of a python script")
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+feedback_path =  script_path + "/FeedbackScripts/"
+#print(script_path)
+
+file_contents = []
+
+for file in os.listdir(feedback_path):
+    # check only text files
+    if file.endswith('.py'):
+        f = open(feedback_path + file, "r")
+        file_content = f"\nprint(\"************\")\nprint(\"{file}\")\n{f.read()}\nprint(\"************\")"
+        file_contents.append(file_content)
+
+collaborated_file_name = script_path + "/allFeedbacks.py"
+
+if os.path.exists(collaborated_file_name):
+    os.remove(collaborated_file_name)
+
+collaborated_code_file = file = open(collaborated_file_name,'a+')
+
+for code_parts in file_contents:
+    collaborated_code_file.write(code_parts)
+collaborated_code_file.close()
