b7fed925b96e152ddb60f05cb87251dea526bc9d
Viktor Bocinec <viktormarko.bocinec@gmail.com>
2022-05-10 10:31:31 +0200

    Diesmal mit Feedback

--- FeedbackScripts/viktorfeedback.py
+++ FeedbackScripts/viktorfeedback.py
@@ -1,1 +1,2 @@
-print("TEST TEST TEST")
+print("POSITIV: Sehr verständlich gut erklärt und auch, die Beispiele sind sehr kreativ gestaltet sodass man es Nachvollziehen kann. Bildlich erklärt ")
+print("NEGATIV: Workshop kommt ein wenig zu spät. Hätten wir ihn früher wäre einiges schneller vorangegangen")
