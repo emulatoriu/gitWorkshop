a9b2b94d1d34a63b06def7bb7ff0e4a25064c5ee
eMu <emad.easa@codecool.com>
2022-06-02 16:37:20 +0200

    moved simu file

--- allFeedbacks.js
+++ allFeedbacks.js
@@ -1,5 +1,56 @@
 
 console.log("************")
+console.log("File: Berniupdate.js")
+console.log("Update Workshop");
+console.log("************")
+console.log("************")
 console.log("File: test.js")
-console.log("test");
+console.log("Hello JavaScript - says Martin");
+
+console.log("************")
+console.log("************")
+console.log("File: Raphael.js")
+
+console.log("************")
+console.log("************")
+console.log("File: JuliasFeedback12.js")
+
+console.log("************")
+console.log("************")
+console.log("File: SimuFeedback.js")
+console.log ("hello");
+console.log("************")
+console.log("************")
+console.log("File: Nenad.js")
+console.log ("Hi);")
+console.log("************")
+console.log("************")
+console.log("File: feedback_alf.js")
+/*
+
+Der Workshop war durchaus ... leider nur verwirrend für mich.
+Ich hoffe, dass sich mir die Vorteile irgendwann erschließen.
+Werde mir das noch anschauen müssen, 
+aber wenn ich nicht gleichzeitig am selben File arbeiten kann,
+ohne bei der Zusammenführung noch einmal eingreifen zu müssen - warum?
+*/
+console.log("hello world");
+console.log("************")
+console.log("************")
+console.log("File: jovan.js")
+console.log("HEllo");
+console.log("************")
+console.log("************")
+console.log("File: angelo.js")
+console.log ("hello there")
+console.log (2+2)
+
+console.log("************")
+console.log("************")
+console.log("File: Berni.js")
+console.log("Hello, der Workshop was sehr hilfreich und informativ aber auch sehr forderdernd"); 
+console.log("************")
+console.log("************")
+console.log("File: amy.js")
+console.log("Leider hat niemand \"Bahnhof\" gesagt");
 console.log("************")
