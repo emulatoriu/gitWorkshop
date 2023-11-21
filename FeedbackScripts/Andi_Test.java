a7637cf84f9bbe1bd70b0dbd1a9b2638c6ab278b
Bandlat <andreas.ratajski@gmail.com>
2023-11-21 11:34:49 +0100

    Just a try

--- FeedbackScripts/Andi_Test.java
+++ FeedbackScripts/Andi_Test.java
@@ -1,7 +1,5 @@
-public class mainCl2
-{
-	public static void main (String[] args)
-	{
-		System.out.println("I kenn mi net aus");
-	}
+public class mainCl2 {
+  public static void main(String[] args) {
+    System.out.println("I kenn mi net aus");
+  }
 }
